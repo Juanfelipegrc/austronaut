675bc6261d4ff2d3772931a1ff6af0fcbf05d08c
Juanfelipegrc <felipegarciarjs@gmail.com>
2025-02-17 21:48:53 -0500

    Code Mode Implemented

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist', 
-    chunkSizeWarningLimit: 1500, 
+    chunkSizeWarningLimit: 2000, 
     rollupOptions: {
       output: {
         manualChunks(id) {
